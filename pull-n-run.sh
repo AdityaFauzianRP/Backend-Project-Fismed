16f64d22626ce112332bb2474a26fa673c048f39
Fadjri <fajriadha35@gmail.com>
2024-10-24 19:34:29 +0700

    etet

--- pull-n-run.sh
+++ pull-n-run.sh
@@ -10,10 +10,10 @@ git checkout main
 docker pull boyangyang/fismed-be:latest
 
 # Stop and remove the fismed-be-dev container if it exists
-if [ "$(docker ps -q -f name=fismed-be-dev)" ]; then
-    docker stop fismed-be-dev
-    docker rm fismed-be-dev
-fi
+# if [ "$(docker ps -q -f name=fismed-be-dev)" ]; then
+#     docker stop fismed-be-dev
+#     docker rm fismed-be-dev
+# fi
 
 # Tag the pulled image as fismed-be:latest
 docker tag boyangyang/fismed-be:latest fismed-be:latest
